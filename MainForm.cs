b55d21fe0a828cf04847176f853f87036cd50259
nekno <nekno@users.noreply.github.com>
2025-05-07 12:49:17 -0500

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel.Design.Serialization;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace SplitTheDiff
+{
+	public partial class MainForm : Form
+    {
+		private static readonly Keys[] disabledKeys = [Keys.OemMinus, Keys.OemPeriod, Keys.Oemcomma, Keys.Decimal, Keys.Subtract];
+
+		public MainForm()
+        {
+            InitializeComponent();
+        }
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			numericUpDown1.SelectAll();
+		}
+		private void NumericUpDown_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (disabledKeys.Contains(e.KeyCode))
+				e.SuppressKeyPress = true;
+		}
+
+		private void NumericUpDown_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode != Keys.Enter)
+				return;
+
+			var numericUpDown = (sender == numericUpDown1) ? numericUpDown2 : numericUpDown1;
+
+			numericUpDown.SelectAll();
+		}
+
+		private void NumericUpDown_ValueChanged(object sender, EventArgs e)
+		{
+			var diff = Math.Abs(numericUpDown1.Value - numericUpDown2.Value) / (decimal)2;
+			diff += Math.Min(numericUpDown1.Value, numericUpDown2.Value);
+			resultLabel.Text = diff.ToString();
+		}
+	}
+
+	public static class NumericUpDownExtensions
+	{
+		public static void SelectAll(this NumericUpDown numericUpDown)
+		{
+			numericUpDown.Focus();
+			numericUpDown.Select(0, numericUpDown.Value.ToString().Length);
+		}
+	}
+}
