8c36c350c329e8f9acc995295b1b1f03a8df6298
nekno <nekno@users.noreply.github.com>
2026-01-19 17:03:57 -0600

    Enable WinForms dark mode

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace SplitTheDiff
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Application.SetColorMode(SystemColorMode.Dark);
             Application.Run(new MainForm());
         }
     }
